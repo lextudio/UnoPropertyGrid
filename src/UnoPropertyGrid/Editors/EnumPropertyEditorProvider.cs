161b747682f665dae36e9f47c08f209faeefcf2d
Lex Li <support@lextm.com>
2026-05-11 20:07:47 -0400

    Fix WinUI 3 enum item name query

--- src/UnoPropertyGrid/Editors/EnumPropertyEditorProvider.cs
+++ src/UnoPropertyGrid/Editors/EnumPropertyEditorProvider.cs
@@ -13,13 +13,19 @@ sealed class EnumPropertyEditorProvider : IPropertyGridEditorProvider
     public FrameworkElement CreateEditor(PropertyGridEditorContext context)
     {
         var type = Nullable.GetUnderlyingType(context.Descriptor.PropertyType) ?? context.Descriptor.PropertyType;
+        // Use string names rather than boxed enum values: WinUI 3 marshals boxed value types
+        // through IReference<T>, causing the WinRT type name to appear as the display text.
         var comboBox = new ComboBox
         {
-            ItemsSource = Enum.GetValues(type).Cast<object>().ToArray(),
-            SelectedItem = context.Value,
+            ItemsSource = Enum.GetNames(type),
+            SelectedItem = context.Value?.ToString(),
             HorizontalAlignment = HorizontalAlignment.Stretch
         };
-        comboBox.SelectionChanged += (_, _) => PropertyGridEditorProviderUtilities.Commit(context, comboBox.SelectedItem);
+        comboBox.SelectionChanged += (_, _) =>
+        {
+            if (comboBox.SelectedItem is string name)
+                PropertyGridEditorProviderUtilities.Commit(context, Enum.Parse(type, name));
+        };
         return comboBox;
     }
 }
