827c6d618204453f6ea84818e3c76cacd76b4aa2
Lex Li <support@lextm.com>
2026-05-27 14:30:43 -0400

    Enable DevFlow for debugging

--- src/UnoPropertyGrid.Sample/App.xaml.cs
+++ src/UnoPropertyGrid.Sample/App.xaml.cs
@@ -1,10 +1,17 @@
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+#if DEBUG
+using LeXtudio.DevFlow.Agent.Uno;
+#endif
 
 namespace UnoPropertyGrid.Sample;
 
 public partial class App : Application
 {
+#if DEBUG
+    private UnoAgentService? _devFlowAgent;
+#endif
+
     public App()
     {
         InitializeComponent();
@@ -12,6 +19,10 @@ public partial class App : Application
 
     protected override void OnLaunched(LaunchActivatedEventArgs args)
     {
+#if DEBUG
+        _devFlowAgent = new UnoAgentService();
+        _devFlowAgent.Start();
+#endif
         var window = new Window();
         window.Content = new Frame();
 
